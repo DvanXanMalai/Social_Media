6c2daf35aa68f4358385602f0a9694324b07d8c7
samyam timsina <88450461+DvanXanMalai@users.noreply.github.com>
2025-05-23 17:26:03 +0545

    Delete backend directory

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { signUp, Login } from '../controllers/authController.js';
-
-const authRouter = express.Router();
-
-authRouter.post('/signup', signUp);
-authRouter.post('/login', Login);
-
-export default authRouter;
--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import authMiddleware from '../middlewares/authMiddleware.js'
-
-const userRouter = express.Router()
-
-userRouter.get('/me', authMiddleware, (req, res) => {
-    res.json({ id: req.user.id, username: req.user.username })
-})
-export default userRouter
